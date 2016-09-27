e4795fd70a7639c4430fdc733ab0b0b5727d0699
14budig <nbudig@gmail.com>
2016-09-27 13:20:13 -0700

    part1

--- starter_code/monster.rb
+++ starter_code/monster.rb
@@ -20,7 +20,7 @@
 # Zombie class
 
   # zombie version of class_description
-  
+
   # initial behavior and values
 
 # Werewolf class
@@ -40,3 +40,70 @@
   # vampire class description
 
   # Flying module included
+
+
+class Monster
+  attr_accessor :habitat, :threat_level, :name
+  @@count=0
+
+  def initialize(name, threat_level = :medium)
+    @threat_level = threat_level
+    @name = name
+    @@count += 1
+    p "Rawr"
+    p "#{@@count} monsters now roam the world!"
+  end
+
+  def self.count
+    @@count
+  end
+
+  def habitat?(test)
+    test == @habitat
+  end
+
+  def self.fight(monster, other_monster)
+    threats = [:low, :medium, :high, :midnight]
+    if threats.index(monster.threat_level) > threats.index(other_monster.threat_level)
+      monster
+    else
+      other_monster
+    end
+  end
+
+  def get_dangerous
+    # ruby version of a switch statement is case
+    case @threat_level
+      when :low
+        @threat_level = :medium
+      when :medium
+        @threat_level = :high
+      when :high
+        @threat_level = :midnight
+      when :midnight
+        :midnight
+    end
+  end
+
+end
+
+
+
+
+
+
+
+
+
+#
+# dracula = Monster.new('Dracula', :high)
+# dracula.habitat = "Transylvania"
+# p dracula.habitat
+# p dracula.threat_level
+# p dracula.habitat?("Transylvania")
+# p dracula.name
+# puts "-----------"
+# teletubby = Monster.new('Teletubby')
+# p teletubby.threat_level
+#
+# p Monster.fight(dracula, teletubby)
